31a18fc55292c78f1b69acf627b9983be10052fe
viktigpetterr <vi1221pe-s@student.lu.se>
2021-05-30 17:44:49 +0200

    Add Alignment.php

---
+++ src/Alignment.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DavidBadura\MarkdownBuilder;
+
+class Alignment
+{
+    public const LEFT = -1;
+    public const CENTER = 0;
+    public const RIGHT = 1;
+}
